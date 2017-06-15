f5a25bcc0ec4cfc6333439c855e540284cfbad99
Atul MalaviyaAtul Malaviya <atulmal@microsoft.com>
2017-06-15 13:46:05 +0530

    Deploy script changed

--- Deploy/deployNodejsApp.sh
+++ Deploy/deployNodejsApp.sh
@@ -1,24 +1,2 @@
-#!/bin/bash
-echo "Updating system..."
 sudo apt-get update
-echo "Start script run"
-sudo apt-get update
-echo "Source update"
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get -qq update
-echo "Install Node"
-apt-get install -y nodejs
-echo "Install Nginx"
-sudo apt-get install -y nginx
-echo "Configure Nginx"
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp node-app-nginx-config /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/node-app-nginx-config /etc/nginx/sites-enabled/node-app-nginx-config
-sudo service nginx restart
-sudo apt-get update
-sudo apt-get install -y npm
-sudo npm install -g pm2
-sudo pm2 start ../server.js
-sudo pm2 startup systemd
-sudo pm2 save
-echo "End of script run"
+echo "all updates installed"
