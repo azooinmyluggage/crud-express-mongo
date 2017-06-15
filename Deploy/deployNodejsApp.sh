4e3db7387489a99ea53c5bdb24ac663cdad06654
Atul MalaviyaAtul Malaviya <atulmal@microsoft.com>
2017-06-15 13:09:21 +0530

    Updated deploy script

--- Deploy/deployNodejsApp.sh
+++ Deploy/deployNodejsApp.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get update
 sudo apt-get install -y nginx
 sudo rm /etc/nginx/sites-enabled/default
 script_dir=$(dirname "$0")
