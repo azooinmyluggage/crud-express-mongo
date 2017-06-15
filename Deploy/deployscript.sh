23895570c68040401d6b3d0086fd9030014a635d
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-06-15 14:27:31 +0530

    Create deployscript.sh

--- Deploy/deployscript.sh
+++ Deploy/deployscript.sh
@@ -1,1 +1,1 @@
-echo "Hello World" && sudo apt-get update && sudo apt-get install -y nodejs
+echo "Hello World" && sudo add-apt-repository main && sudo apt-get update && sudo apt-get install -y nginx && sudo apt-get install -y nodejs
